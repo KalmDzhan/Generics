3af2e6dcb2afaf5e03f952fa9aca6a56887b95d0
DzhangrErdniev21D <85980107+KalmDzhan@users.noreply.github.com>
2023-05-10 11:28:07 +0300

    Add files via upload

---
+++ Generics/Generics/CustomArray.cs
@@ -0,0 +1,67 @@
+﻿namespace Generics
+{
+    public class CustomArray<TItem>
+    {
+        private TItem[] items;
+
+        public CustomArray(int size)
+        {
+            items = new TItem[size];
+        }
+
+        public void Add(TItem item)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (EqualityComparer<TItem>.Default.Equals(items[i], default(TItem)))
+                {
+                    items[i] = item;
+                    return;
+                }
+            }
+            throw new IndexOutOfRangeException("Массив переполнен");
+        }
+
+        public void Delete(int index)
+        {
+            if (index < 0 || index >= items.Length)
+            {
+                throw new IndexOutOfRangeException("Индекс вне массива");
+            }
+            items[index] = default(TItem);
+        }
+
+        public TItem FindFirst(Func<TItem, bool> filter)
+        {
+            foreach (TItem item in items)
+            {
+                if (filter(item))
+                {
+                    return item;
+                }
+            }
+            return default(TItem);
+        }
+
+        public void Print()
+        {
+            foreach (TItem item in items)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Swap(int index1, int index2)
+        {
+            if (index1 < 0 || index1 >= items.Length || index2 < 0 || index2 >= items.Length)
+            {
+                throw new IndexOutOfRangeException("Индекс вне массива");
+            }
+            TItem temp = items[index1];
+            items[index1] = items[index2];
+            items[index2] = temp;
+        }
+    }
+
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,22 @@
+﻿using Generics;
+
+class Program
+{
+    static void Main()
+    {
+        CustomArray<int> array = new CustomArray<int>(5);
+        array.Add(1);
+        array.Add(2);
+        array.Add(3);
+        array.Print(); // 1 2 3
+
+        array.Delete(1);
+        array.Print(); // 1 0 3
+
+        int first = array.FindFirst(x => x > 1);
+        Console.WriteLine(first); // 3
+
+        array.Swap(0, 2);
+        array.Print(); // 3 0 1
+    }
+}
